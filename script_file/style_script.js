953cb3fc8328f462889c8f165d5cbb4529052fd6
kuttydhilip <kuttydhilip543@gmail.com>
2024-01-01 01:49:04 +0530

    script write for screen response & anime response & mouse hover response

--- script_file/style_script.js
+++ script_file/style_script.js
@@ -2,12 +2,24 @@
 const menu = document.querySelector('#menu');
 const menuList = document.querySelector('nav ul');
 const logo = document.querySelector('.logo');
+const topNav = document.querySelector('.animeTop')
 menu.addEventListener('click', () => {
     menuList.classList.toggle('showmenu');
     logo.classList.toggle('showlogo');
+    topNav.classList.toggle('animeTop');
+    topNav.classList.toggle('animeLeft');
+    
+
 });
 window.addEventListener('resize', () => {
     menuList.classList.remove('showmenu');
     logo.classList.remove('showlogo');
+    topNav.classList.add('animeTop');
+    topNav.classList.remove('animeLeft');
+
 });
 // ************************************************************************************************************
+window.addEventListener('scroll',() =>{
+    menuList.classList.remove('showmenu');
+    logo.classList.remove('showlogo');
+});
